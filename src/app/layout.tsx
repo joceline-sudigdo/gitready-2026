98c393d93f2b1943347c9d22a04b7a782f60dc6b
Joceline sudigdo <jocesudigdo947@gmail.com>
2026-08-07 20:04:03 +0700

    chore: initial project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: [
+    "100",
+    "200",
+    "300",
+    "400",
+    "500",
+    "600",
+    "700",
+    "800",
+    "900",
+  ],
+    variable: "--font-poppins",
+    display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "GitReady 2026",
+  description: "GitReady Workshop Landing Page",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
